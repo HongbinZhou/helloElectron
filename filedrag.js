e1cfe8d74f1d1d3d7990fc718b40ee45d13e9de8
Hongbin Zhou <hongbinzhou@hotmail.com>
2016-08-12 23:52:26 +0800

    add xlsx support.

--- filedrag.js
+++ filedrag.js
@@ -53,6 +53,20 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 			"</strong> bytes</p>"
 		);
 
+      // check: https://github.com/SheetJS/js-xlsx
+      if(typeof require !== 'undefined') XLSX = require('xlsx');
+      var workbook = XLSX.readFile('./test/test.xlsx');
+
+      var sheet_name_list = workbook.SheetNames;
+      sheet_name_list.forEach(function(y) { /* iterate through sheets */
+          var worksheet = workbook.Sheets[y];
+          for (z in worksheet) {
+              /* all keys that do not begin with "!" correspond to cell addresses */
+              if(z[0] === '!') continue;
+              console.log(y + "!" + z + "=" + JSON.stringify(worksheet[z].v));
+          }
+      });
+
 	}
 
 
